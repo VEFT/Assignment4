a4af2fcb3ee8714f923de3ca69aa65ccb95ba416
Daníel Benediktsson <danielb13@ru.is>
2015-09-15 03:04:17 -0700

    flot

--- Assignment4-startproject/Week05/CoursesAPI.Services/Utilities/DateTimeUtils.cs
+++ Assignment4-startproject/Week05/CoursesAPI.Services/Utilities/DateTimeUtils.cs
@@ -2,10 +2,16 @@
 {
 	public class DateTimeUtils
 	{
+        /// <summary>
+        /// Function that tells if a year is a leap year or not.
+        /// If the function returns true than the year is leap year
+        /// but false otherwise.
+        /// </summary>
+        /// <param name="year">The year</param>
+        /// <returns>True if the year is leap year but false otherwise</returns>
 		public static bool IsLeapYear(int year)
 		{
-			// TODO: implement!
-			return false;
+            return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 		}
 	}
 }
