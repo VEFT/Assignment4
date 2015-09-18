692cf1cb3dd26da9d2836e2825e881bc7453e1a3
Daníel Benediktsson <danielb13@ru.is>
2015-09-18 09:42:42 -0700

    flot

--- Assignment4-startproject/Week05/CoursesAPI.Tests/Services/CourseServicesTests.cs
+++ Assignment4-startproject/Week05/CoursesAPI.Tests/Services/CourseServicesTests.cs
@@ -121,7 +121,7 @@ namespace CoursesAPI.Tests.Services
             CoursesServiceProvider _serviceWithNoDataDefined = new CoursesServiceProvider(_mockUnitOfWork);
             const string SEMESTER = "20153";
 
-			// Act:
+            // Act:
             var result = _serviceWithNoDataDefined.GetCourseInstancesBySemester(SEMESTER);
 
             // Assert:
@@ -129,20 +129,27 @@ namespace CoursesAPI.Tests.Services
 		}
 
 		[TestMethod]
-        public void test1()
+        public void ble()
         {
             // todo
+            // 
+            // todo
+            // todo
+            // todo
+            // todo
+            // todo
+            // todotodo
         }
 
-		#endregion
+        #endregion
 
-		#region AddTeacher
+        #region AddTeacher
 
-		/// <summary>
-		/// Adds a main teacher to a course which doesn't have a
-		/// main teacher defined already (see test data defined above).
-		/// </summary>
-		[TestMethod]
+        /// <summary>
+        /// Adds a main teacher to a course which doesn't have a
+        /// main teacher defined already (see test data defined above).
+        /// </summary>
+        [TestMethod]
 		public void AddTeacher_WithValidTeacherAndCourse()
 		{
 			// Arrange:
